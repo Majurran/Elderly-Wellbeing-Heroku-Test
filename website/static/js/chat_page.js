27b623be3375f201b9ae4146c31fe590c3480bb9
Majurran <majurran@gmail.com>
2022-10-18 10:28:13 +1000

    Fixed Errors with chat

--- website/static/js/chat_page.js
+++ website/static/js/chat_page.js
@@ -1,10 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-    // Make sidebar collapse on click
-    document.querySelector('#show-sidebar-button').onclick = () => {
-        document.querySelector('#sidebar').classList.toggle('view-sidebar');
-    };
-
     // Make 'enter' key submit message
     let msg = document.getElementById("user_message");
     msg.addEventListener("keyup", function(event) {
